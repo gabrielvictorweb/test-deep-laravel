1738854df3c06ed9220aece55414fec86ca4f86f
Gabriel Victor <gabrielvictorweb@outlook.com>
2026-04-19 04:26:50 -0300

    build(tailwind): configure flowbite plugin and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./resources/views/**/*.blade.php", "./resources/js/**/*.js"],
+    content: [
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+        "./node_modules/flowbite/**/*.js",
+    ],
     theme: {
         extend: {
             colors: {
+                "blue-gray": {
+                    50: "#f8fafc",
+                    100: "#f1f5f9",
+                    200: "#e2e8f0",
+                    300: "#cbd5e1",
+                    400: "#94a3b8",
+                    500: "#64748b",
+                    600: "#475569",
+                    700: "#334155",
+                    800: "#1e293b",
+                    900: "#0f172a",
+                    950: "#020617",
+                },
                 dark: {
                     bg: "#080a0f",
                     container: "#0d1017",
@@ -21,5 +40,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [flowbite],
 };
