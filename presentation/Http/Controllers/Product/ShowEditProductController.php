58ec0d4e542a7660ec90bcc8b0bf25680d3181aa
Gabriel Victor <gabrielvictorweb@outlook.com>
2026-04-19 04:27:07 -0300

    feat(controller): add dedicated product edit page controller

---
+++ presentation/Http/Controllers/Product/ShowEditProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Presentation\Http\Controllers\Product;
+
+use Application\UseCases\User\FindRegisteredUserUseCase;
+use Domain\Contracts\UserRepositoryInterface;
+use Domain\Models\Product;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Presentation\Http\Controllers\Controller;
+
+class ShowEditProductController extends Controller
+{
+    public function execute(
+        Product $product,
+        FindRegisteredUserUseCase $findRegisteredUserUseCase,
+        UserRepositoryInterface $userRepository,
+    ): View|RedirectResponse {
+        $registeredUser = $findRegisteredUserUseCase->execute(auth()->user(), $userRepository);
+
+        if ($registeredUser === null) {
+            return redirect()
+                ->route('users.create')
+                ->with('warning', 'Conclua seu cadastro de usuario para editar produtos.');
+        }
+
+        abort_if($product->user_id !== $registeredUser->id, 403);
+
+        $product->loadMissing('images');
+
+        return view('products.edit', [
+            'product' => $product,
+            'registeredUser' => $registeredUser,
+        ]);
+    }
+}
