29cab177bd246f0c319ec5f6c5cb6627fc831b00
Gabriel Victor <gabrielvictorweb@outlook.com>
2026-04-19 04:27:07 -0300

    feat(model): add product image model

---
+++ domain/Models/ProductImage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductImage extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'path',
+        'url',
+        'sort_order',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
